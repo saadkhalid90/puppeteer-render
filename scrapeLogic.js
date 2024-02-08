113e6cacc880970dacc5a530cde66d99a9798c70
Saad Khalid <saadkhalid90@gmail.com>
2024-02-08 01:53:30 -0500

    browser scope fixed

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,19 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function scrapeLogic(req, res) {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
   try {
-    const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
-    });
     const page = await browser.newPage();
 
     // Navigate the page to a URL
